2dae58ac0a1acfa647a7102dafa8d542bb5c60b5
Joonatan Linkola <joonatan.linkola@gmail.com>
2022-06-24 11:31:30 +0300

    tehtävä 3.3

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 let persons = [
   {
+    id: 1,
     name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
+    number: "040-123456"
   },
   {
+    id: 2,
     name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
+    number: "39-44-5323523"
   },
   {
+    id: 3,
     name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3
+    number: "12-43-234345"
   },
   {
+    id: 4,
     name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4
+    number: "39-23-6423122"
   }
 ]
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(persons))
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  response.send(`<div>
+                  Phonebook has info for ${persons.length} people
+                  <p>
+                    ${Date()}
+                  <p/>
+                </div>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(each => each.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+  
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
